ed9a3e4afd78127c9017c1a547a91348ef5d3bfd
BE A STEREOTYPE <judiction@gmail.com>
2026-06-19 14:35:53 -0300

    V1 Praia Copacabana Coluna 3

---
+++ data.js
@@ -0,0 +1,269 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sala-de-estar",
+      "name": "Sala de Estar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5571695567075174,
+        "pitch": 0.32700729427126696,
+        "fov": 1.505333938584494
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2695840127799283,
+          "pitch": 0.526000955765241,
+          "rotation": 4.71238898038469,
+          "target": "3-mesa-de-trabalho"
+        },
+        {
+          "yaw": 1.217797166393785,
+          "pitch": 0.1985674426213926,
+          "rotation": 0,
+          "target": "2-quarto-e-varanda"
+        },
+        {
+          "yaw": -2.3167940537756735,
+          "pitch": 0.3287802245949809,
+          "rotation": 7.853981633974483,
+          "target": "1-cozinha"
+        },
+        {
+          "yaw": -2.14173333947166,
+          "pitch": 0.12607697202587076,
+          "rotation": 7.0685834705770345,
+          "target": "4-banheiro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cozinha",
+      "name": "Cozinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.8183654937128644,
+        "pitch": 0.27580604696668054,
+        "fov": 1.505333938584494
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2798778571901739,
+          "pitch": 0.37677374353780735,
+          "rotation": 5.497787143782138,
+          "target": "0-sala-de-estar"
+        },
+        {
+          "yaw": 1.2559495673269012,
+          "pitch": 0.22732699875939488,
+          "rotation": 4.71238898038469,
+          "target": "3-mesa-de-trabalho"
+        },
+        {
+          "yaw": -1.76770521613515,
+          "pitch": 0.14891091133753065,
+          "rotation": 7.853981633974483,
+          "target": "4-banheiro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-quarto-e-varanda",
+      "name": "Quarto e Varanda",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.0376798006143737,
+        "pitch": 0.25193651490194746,
+        "fov": 1.505333938584494
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.907823272036543,
+          "pitch": 0.4059611504129901,
+          "rotation": 1.5707963267948966,
+          "target": "3-mesa-de-trabalho"
+        },
+        {
+          "yaw": 2.928655947119764,
+          "pitch": 0.16599489695501646,
+          "rotation": 0.7853981633974483,
+          "target": "0-sala-de-estar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-mesa-de-trabalho",
+      "name": "Mesa de Trabalho",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.19134348746246843,
+        "pitch": 0.5098322921260419,
+        "fov": 1.505333938584494
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.514724581626048,
+          "pitch": 0.33168690462365724,
+          "rotation": 5.497787143782138,
+          "target": "2-quarto-e-varanda"
+        },
+        {
+          "yaw": -2.939588179679543,
+          "pitch": 0.5561330216647011,
+          "rotation": 1.5707963267948966,
+          "target": "0-sala-de-estar"
+        },
+        {
+          "yaw": -2.799555150506265,
+          "pitch": 0.12156505734345835,
+          "rotation": 7.0685834705770345,
+          "target": "1-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-banheiro",
+      "name": "Banheiro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.08680670765106058,
+        "pitch": 0.4372017990353054,
+        "fov": 1.505333938584494
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7128992325298382,
+          "pitch": 0.15708539150991818,
+          "rotation": 4.71238898038469,
+          "target": "1-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Be.in.Rio Praia Copacabana Coluna 3",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
